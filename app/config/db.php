9c933f68f21c75e0b3f7bdb6d20a592a96f55d27
hogvarce <hogvarce@gmail.com>
2016-03-11 22:16:04 +0300

    first commit

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=milih-plastic',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
